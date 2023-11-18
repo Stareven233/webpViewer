90fc269de87aa667619e60b0011eb8db5b594b77
Noetu <46963288+Stareven233@users.noreply.github.com>
2023-11-18 12:16:29 +0800

    feat: add TextPanel

--- src/components/DataPanel.tsx
+++ src/components/DataPanel.tsx
@@ -5,15 +5,28 @@ import { Dynamic } from "solid-js/web"
 import neoStore from "../store"
 import { getFile } from '../api'
 
+const ImagePanel: Component<any> = props => {
+  return <img class="object-contain object-center mx-auto max-h-full" src={props.url} alt={props.name} />
+}
+
+const TextPanel: Component<any> = props => {
+  return (
+    <section class='max-h-full overflow-y-scroll text-left justify-normal py-3'>
+      <pre class="mx-auto break-words whitespace-pre-wrap">{props.text}</pre>
+    </section>
+  )
+}
+
 const Comp: Component = () => {
   const { store } = neoStore
   const [blob] = createResource(() => store.currentItem, getFile)
   const blobURL = createMemo(() => URL.createObjectURL(new Blob([blob()])))
+  const [blobText] = createResource(() => blob(), async b => await b.text())
   const dataType = createMemo(() => blob()?.type.split('/')[0])
-
-  const ImagePanel = () => <img class="object-contain object-center mx-auto max-h-full" src={blobURL()} alt={store.currentItem} />
   const options = {
-    image: ImagePanel,
+    image: () => <ImagePanel url={blobURL()} name={store.currentItem} />,
+    text: () => <TextPanel text={blobText()} />,
+    application: () => <TextPanel text={blobText()} />,
   }
   const PanelFallback = <p class='text-2xl'>暂不支持的类型: {dataType()}</p>
 
